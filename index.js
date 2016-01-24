70d236b92494a24ef678e59ae9a62c268628a30f
Mark Vayngrib <mark@tradle.io>
2016-01-23 22:24:21 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,237 @@
+
+var EventEmitter = require('events').EventEmitter
+var util = require('util')
+var debug = require('debug')('websocket-relay')
+var Q = require('q')
+var http = require('http')
+var typeforce = require('typeforce')
+// var clone = require('xtend')
+var io = require('socket.io')
+var constants = require('@tradle/constants')
+
+function Server (opts) {
+  var self = this
+
+  typeforce({
+    port: 'Number',
+    path: '?String',
+    // byRootHash: 'Function'
+  }, opts)
+
+  EventEmitter.call(this)
+
+  this._queues = {}
+  this._pendingHandshakes = {}
+  this._socketsByRootHash = {}
+  // this._lookup = opts.byRootHash
+  this._server = http.createServer(function (req, res) {
+    res.writeHead(500)
+    res.end('This is a websockets endpoint!')
+  })
+
+  this._server.listen(opts.port)
+  this._io = io(this._server, { path: opts.path || '/' }) //.of('/' + opts.rootHash)
+  this._io.on('connection', function (socket) {
+    var clientRootHash
+    socket.on('error', function (err) {
+      debug('socket for client', clientRootHash, 'experienced error', err)
+      socket.disconnect()
+    })
+
+    // TODO: handshake before allowing them to subscribe
+    socket.once('subscribe', function (rootHash) {
+      clientRootHash = rootHash
+      self.emit('connect', clientRootHash)
+
+      self._socketsByRootHash[clientRootHash] = socket
+      socket._tradleRootHash = clientRootHash
+      socket.on('message', function (msg, cb) {
+        try {
+          typeforce({
+            to: 'String',
+            message: 'String'
+          }, msg)
+        } catch (err) {
+          debug('received invalid message from', clientRootHash, err)
+          return socket.emit('error', { message: 'invalid message', data: msg })
+        }
+
+        msg.from = clientRootHash
+        msg.callback = cb
+        self._forwardMessage(msg)
+      })
+    })
+
+    socket.once('disconnect', function () {
+      if (clientRootHash) {
+        self.emit('disconnect', clientRootHash)
+      }
+
+      var rh = socket._tradleRootHash
+      if (rh) delete self._socketsByRootHash[rh]
+    })
+
+    // socket.once('join', function (data) {
+    //   clientRootHash = data[ROOT_HASH]
+    //   debug('initiating handshake with', clientRootHash)
+    //   return self._handshake(socket, clientRootHash)
+    //     .catch(function (err) {
+    //       socket.disconnect()
+    //     })
+    // })
+  })
+}
+
+util.inherits(Server, EventEmitter)
+module.exports = Server
+
+// Server.prototype._handshake = function (socket, rootHash) {
+//   var self = this
+//   var pending = this._pendingHandshakes[rootHash]
+//   if (pending) return pending.promise
+
+//   var serverNonce
+//   var identityInfo
+//   var verifyingKey
+//   var challengeRespDefer = Q.defer()
+//   var handshakeDefer = Q.defer()
+//   socket.once('disconnect', function () {
+//     challengeRespDefer.reject()
+//     handshakeDefer.reject()
+//   })
+
+//   socket.once('handshake', challengeRespDefer.resolve)
+//   this.once('welcome:' + rootHash, handshakeDefer.resolve)
+
+//   this._lookup(rootHash)
+//     .then(function (result) {
+//       identityInfo = result
+//       var pubkeys = result.identity.toJSON().pubkeys
+//       verifyingKey = pubkeys.filter(function (k) {
+//         return k.type === 'ec' && k.purpose === 'sign'
+//       })[0]
+
+//       serverNonce = crypto.randomBytes(32).toString('hex')
+//       debug('handshake 1. sending challenge', rootHash)
+//       socket.emit('handshake', {
+//         serverNonce: serverNonce,
+//         key: verifyingKey.fingerprint
+//       })
+
+//       return Q.race([
+//         Q.Promise(function (resolve, reject) {
+//           setTimeout(function () {
+//             reject(new Error('handshake timed out'))
+//           }, HANDSHAKE_TIMEOUT).unref()
+//         }),
+//         challengeRespDefer.promise
+//       ])
+//     })
+//     .then(function (data) {
+//       debug('handshake 2. received challenge response from', rootHash)
+//       if (data.serverNonce !== serverNonce) {
+//         throw new Error('nonce mismatch')
+//       }
+
+//       var sig = data[SIG]
+//       if (!sig) throw new Error('missing signature')
+
+//       delete data[SIG]
+//       var responseStr = utils.stringify(data)
+//       verifyingKey = kiki.toKey(verifyingKey)
+//       if (!verifyingKey.verify(responseStr, sig)) {
+//         throw new Error('client gave bad signature')
+//       }
+//     })
+//     .then(function () {
+//       debug('handshake 3. client authenticated', rootHash)
+
+//       // http://stackoverflow.com/questions/17476294/how-to-send-a-message-to-a-particular-client-with-socket-io#comment39387846_17535099
+//       // each customer joins the room corresponding to their ROOT_HASH
+//       self._addSocket[rootHash]
+//       // corresponding client side code:
+//       // var socket = io.connect('http://localhost');
+//       // socket.emit('join', {[ROOT_HASH]: '...'})
+//     })
+//     .catch(function (err) {
+//       console.error(err, err.stack)
+//       throw err
+//     })
+
+//   this._pendingHandshakes[rootHash] = {
+//     socket: socket,
+//     promise: handshakeDefer.promise
+//   }
+
+//   return handshakeDefer.promise
+// }
+
+// Server.prototype._addSocket = function (rootHash, socket) {
+//   var self = this
+//   delete this._pendingHandshakes[rootHash]
+//   this._socketsByRootHash[rootHash] = socket
+//   socket.join(rootHash)
+//   socket.emit('welcome')
+//   socket.on('message', function (msg, cb) {
+//     var toRootHash = msg.toRootHash
+//     debug('forwarding msg from', rootHash, 'to', toRootHash)
+//     self._forwardMessage({
+//       form: rootHash,
+//       to: toRootHash,
+//       message: msg.data,
+//       callback: cb
+//     })
+//   })
+
+//   this.emit('welcome:' + rootHash)
+
+//   var queued = this._queues[rootHash]
+//   if (queued && queued.length) {
+//     queued = queued.slice()
+//     this._queued[rootHash].length = 0
+//     queued.forEach(self._forwardMessage, self)
+//   }
+// }
+
+Server.prototype.getConnectedClients = function () {
+  return Object.keys(this._socketsByRootHash)
+}
+
+Server.prototype._forwardMessage = function (msgInfo) {
+  typeforce({
+    from: 'String',
+    to: 'String',
+    message: 'String',
+    callback: '?Function'
+  }, msgInfo)
+
+  var to = msgInfo.to
+  var socket = this._socketsByRootHash[to]
+  if (socket) {
+    return socket.emit('message', {
+      from: msgInfo.from,
+      message: msgInfo.message
+    }, msgInfo.callback)
+  }
+
+  var queue = this._queues[to] = this._queues[to] || []
+  queue.push(msgInfo)
+}
+
+Server.prototype.destroy = function () {
+  if (this._destroyed) return Q.reject(new Error('already destroyed'))
+
+  this._destroyed = true
+  debug('destroying')
+
+  for (var rootHash in this._socketsByRootHash) {
+    var s = this._socketsByRootHash[rootHash]
+    s.disconnect()
+    // s.removeAllListeners()
+  }
+
+  delete this._socketsByRootHash
+  this._io.close()
+  this._server.close()
+  return Q()
+}
---
+++ test/index.js
@@ -0,0 +1,132 @@
+
+var path = require('path')
+var test = require('tape')
+var Q = require('q')
+var ROOT_HASH = require('@tradle/constants').ROOT_HASH
+var utils = require('@tradle/utils')
+var DSA = require('@tradle/otr').DSA
+var WebSocketRelay = require('../')
+var WebSocketClient = require('@tradle/ws-client')
+var billPub = require('./fixtures/bill-pub')
+var billPriv = require('./fixtures/bill-priv')
+var tedPub = require('./fixtures/ted-pub')
+var tedPriv = require('./fixtures/ted-priv')
+var rufusPub = require('./fixtures/rufus-pub')
+var rufusPriv = require('./fixtures/rufus-priv')
+var BASE_PORT = 22222
+var billRootHash
+var tedRootHash
+var rufusRootHash
+var people = {}
+
+test('setup', function (t) {
+  Q.all([billPub, tedPub, rufusPub].map(function (identity) {
+    return Q.ninvoke(utils, 'getStorageKeyFor', new Buffer(utils.stringify(identity)))
+  })).spread(function (b, t, r) {
+    b = b.toString('hex')
+    t = t.toString('hex')
+    r = r.toString('hex')
+
+    billRootHash = b
+    tedRootHash = t
+    rufusRootHash = r
+
+    people[b] = people.bill = {
+      pub: billPub,
+      priv: billPriv
+    }
+
+    people[t] = people.ted = {
+      pub: tedPub,
+      priv: tedPriv
+    }
+
+    people[r] = people.rufus = {
+      pub: rufusPub,
+      priv: rufusPriv
+    }
+  })
+  .done(t.end)
+})
+
+test('websockets with relay', function (t) {
+  var port = BASE_PORT++
+
+  var relayPath = '/custom/relay/path'
+  var relay = new WebSocketRelay({
+    port: port,
+    path: relayPath
+  })
+
+  var relayURL = 'http://127.0.0.1:' + port + path.join('/', relayPath)
+  var rufus = new WebSocketClient({
+    url: relayURL,
+    otrKey: getDSAKey(rufusPriv),
+    rootHash: rufusRootHash,
+  })
+
+  var bill = new WebSocketClient({
+    url: relayURL,
+    otrKey: getDSAKey(billPriv),
+    rootHash: billRootHash,
+  })
+
+  var rufusInfo = {
+    identity: rufusPub
+  }
+
+  rufusInfo[ROOT_HASH] = rufusRootHash
+
+  var billInfo = {
+    identity: billPub
+  }
+
+  billInfo[ROOT_HASH] = billRootHash
+
+  var togo = 2
+
+  bill.send(rufusRootHash, toBuffer({
+    hey: 'rufus'
+  }), rufusInfo).done()
+
+  rufus.send(billRootHash, toBuffer({
+    hey: 'bill'
+  }), billInfo).done()
+
+  bill.on('message', function (msg) {
+    done()
+    t.equal(JSON.parse(msg).hey, 'bill')
+  })
+
+  rufus.on('message', function (msg) {
+    done()
+    t.equal(JSON.parse(msg).hey, 'rufus')
+  })
+
+  function done () {
+    if (--togo) return
+
+    Q.all([
+      relay.destroy(),
+      bill.destroy(),
+      rufus.destroy()
+    ]).done(function () {
+      t.end()
+      // Socket.IO takes ~30 seconds to clean up (timeout its connections)
+      // no one wants to wait that long for tests to finish
+      process.exit(0)
+    })
+  }
+})
+
+function toBuffer (obj) {
+  return new Buffer(utils.stringify(obj))
+}
+
+function getDSAKey (keys) {
+  var key = keys.filter(function (k) {
+    return k.type === 'dsa'
+  })[0]
+
+  return DSA.parsePrivate(key.priv)
+}
